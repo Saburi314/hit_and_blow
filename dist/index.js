8921f6c2d1154983575abf05d88bb381f3bb67d2
Saburi <nnta20yy@docomo.ne.jp>
2025-03-27 22:18:01 +0900

    console.logをprocess.stdoutに修正

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,8 @@
 "use strict";
+exports.__esModule = true;
+exports.sayHello = void 0;
 var sayHello = function (name) {
     return "Hello, " + name + "!";
 };
-console.log(sayHello('Saburi Yuta'));
+exports.sayHello = sayHello;
+process.stdout.write(exports.sayHello('Saburi Yuta'));
